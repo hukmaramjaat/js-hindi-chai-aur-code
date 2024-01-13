d7322e8247d6639b79d6817383748df84b04f15b
Hukma ram jat <111831531+hukmaramjaat@users.noreply.github.com>
2024-01-13 18:59:30 +0000

    datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+
+let age = 18
+let isLoggedIn = false
+let state;
+
+// numer => 2 to power 53
+// biging 
+// string => ""
+// Boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accoutnId = 1444
+let accountEmail = "hukmaram@gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+
+// accountId = 2  not allowed
+
+accountEmail = "hc@gmail.com"
+accountPassword = '21212'
+
+/*
+Perfer not to use var
+because of issue in block scope and functional scop
+*/
+console.log(accoutnId)
