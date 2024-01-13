ddb7279a85e671770c3f9617a760ea3b7d6df86c
Hukma ram jat <111831531+hukmaramjaat@users.noreply.github.com>
2024-01-13 19:34:37 +0000

    conversion Operation learn

---
+++ basics/conversionOperation.js
@@ -0,0 +1,28 @@
+// "33" => 33
+// "33abd" => NaN
+// ture => 1; false => 0
+
+
+let score = "hukma"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLoggedIn = "hukma"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// " " => false
+// "hukma" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
