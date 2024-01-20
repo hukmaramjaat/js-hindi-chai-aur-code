dbb016df29540bc70d7fd8ff61a50a95e3864e7e
Hukma ram jat <111831531+hukmaramjaat@users.noreply.github.com>
2024-01-20 21:10:15 +0000

    some file added

---
+++ iterations/three.js
@@ -0,0 +1,18 @@
+//  for of
+
+// const arr = [1,2,3,4,5];
+
+// for (const num of arr) {
+//     console.log(num)
+// }
+
+const greeting = "helloworld!";
+
+for (const greet of greeting) {
+    console.log(greet);
+}
+
+// Maps
+
+const map = num Map();
+map.
