ed148babd50a78c81824f4b29a0fdc4ebf7192ff
Hukma ram jat <111831531+hukmaramjaat@users.noreply.github.com>
2024-01-21 10:05:18 +0000

    forEach and forin loop added

---
+++ iterations/five.js
@@ -0,0 +1,39 @@
+const coding = ["js", "ruby", "java", "python", "cpp"];
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item)
+// })
+
+// function printMe(item) {
+//     console.log(item);
+// }
+// coding.forEach(printMe);
+
+// coding.forEach( (item, index, arr) =>{
+//     console.log(item, index, arr)
+// })
+
+
+const mycoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "C++",
+        languageFileName: "cpp"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    }
+]
+
+mycoding.forEach( (item) => {
+    console.log(item.languageName);
+})
+
---
+++ iterations/four.js
@@ -0,0 +1,20 @@
+const myObject = {
+    js: 'javaScript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: "Swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]} `)
+}
+
+const programming = ["js", "rb", "py","java","cpp"];
+
+for (const key in programming) {
+//     console.log(key);
+//     console.log(programming[key])
+}
+
+// map ke upr for in loop not iterable
+
