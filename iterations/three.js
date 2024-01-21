a475ec9283338b338ee5806d1c2ce71fa17cbc2a
Hukma ram jat <111831531+hukmaramjaat@users.noreply.github.com>
2024-01-21 08:34:07 +0000

    loops added

--- iterations/three.js
+++ iterations/three.js
@@ -1,5 +1,7 @@
 //  for of
 
+// [{}, {}, {}]
+
 // const arr = [1,2,3,4,5];
 
 // for (const num of arr) {
@@ -14,5 +16,29 @@ for (const greet of greeting) {
 
 // Maps
 
-const map = num Map();
-map.
+const map = new Map();
+map.set('IN', "India");
+map.set("USA", "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+// const map = new Map();
+// map.set('Jp', "jaiput");
+// console.log(map);
+
+// for (const iterator of map) {
+//     console.log(iterator);
+// }
+
+// for (const [key, value] of map) {
+    // console.log(key, ":->", value);
+// }
+
+const myObject = {
+    'game1': 'NFS',
+    'game2': 'Spiderman',
+    'game3': 'BGMI LIGHT'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ":-", value)
+// }  not iterable for object
